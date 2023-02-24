4c1aaa0da560bcaf45867f4082ccdb2032ebf441
DKorjey <arsushabelor@icloud.com>
2023-02-24 22:40:15 +0300

    Main commit

---
+++ examples/skinstats.js
@@ -0,0 +1,31 @@
+// Here example how getSkinById() and getLike() works
+// User writes id of the skin, and programm write skin stats
+
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output, exit } from "process";
+import { getLike, getSkinById } from "../index.js"; // P.S. In real code use "node-trss-api" instead of "../index.js"
+
+function fromUpper(str) {
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+
+const rl = readline.createInterface({ input, output });
+
+const id = parseInt(await rl.question("Enter skin id: "));
+
+rl.close();
+
+if (isNaN(id)) {
+  console.log("Enter a number in next time");
+  exit(1);
+}
+
+const skinStats = await getSkinById(id)
+
+console.log("Result: ")
+
+for (const k in skinStats) {
+  console.log(fromUpper(k).replace(/_/g, " ") + ": " + skinStats[k])
+}
+
+console.log("Self-like: ", getLike(skinStats.author_id, id) ? "Yes" : "No")
--- index.js
+++ index.js
@@ -1,7 +1,46 @@
 import fetch from "node-fetch";
+import { deflateRawSync, inflateRawSync } from "zlib";
+import _ from "lodash";
+const { chunk } = _;
+import chroma from "chroma-js";
 
-export const usersLink = mainLink + "users.php";
-export const skinsLink = mainLink + "skins.php";
+/**
+ * @typedef {Object} SkinInfo
+ * @property {number} author_id
+ * @property {number} id
+ * @property {number} likes
+ * @property {string} primary_color
+ * @property {string} secondary_color
+ * @property {string} skin
+ * @property {string} skin_name
+ * @property {number} views
+ */
+
+/**
+ * @typedef {Object} UserInfo
+ * @property {number} id
+ * @property {string} login
+ * @property {string} skin
+ * @property {string} primary_color
+ * @property {string} secondary_color
+ */
+
+/**
+ * @typedef {Object} UserInfoByToken
+ * @property {number} id
+ * @property {string} login
+ * @property {string} skin
+ * @property {string} private_token
+ * @property {string} primary_color
+ * @property {string} secondary_color
+ */
+
+/**
+ * @typedef {Object} ChangedSkin
+ * @property {string} skin
+ * @property {string} primary_color
+ * @property {string} secondary_color
+ */
 
 export const links = {
   mainLink: "http://trsstest.crystalcloud.xyz/game-dev/TRSSDatabase/",
@@ -9,7 +48,29 @@ export const links = {
   skinsLink: "http://trsstest.crystalcloud.xyz/game-dev/TRSSDatabase/skins.php",
 };
 
-export const actions = {login: "login", getUserById: "get_user_by_id"}
+Object.freeze(links);
+
+export const actions = {
+  login: "login",
+  getUserById: "get_user_by_id",
+  getUserByToken: "get_user_by_token",
+  changeUserSkin: "change_skin",
+  toggleLike: "toggle_like",
+  registerView: "register_view",
+  getLike: "get_like",
+  uploadSkin: "upload_skin",
+  removeSkin: "remove_skin",
+  getSkinById: "get_skin_by_id",
+  getSkinsById: "get_skins_by_id",
+  getRecentSkins: "get_recent_skins",
+  getSkinsByAuthorId: "get_skins_by_author_id",
+  getSkinsByLikes: "get_skins_by_likes",
+  getSkinsByViews: "get_skins_by_views",
+  getLikes: "get_likes",
+  getViews: "get_views",
+};
+
+Object.freeze(actions);
 
 export const trssErrors = {
   0: "Invalid user or password",
@@ -20,17 +81,14 @@ export const trssErrors = {
   5: "Exactly the same skin from the same author is already in the database",
   6: "Skin not found",
   7: "Skin creator ID does not match user ID",
+  8: "Incorrect value",
   "-1": "Unknown error",
 };
 
-/**
- * @typedef {(string)} token
- * @typedef {(string[][])} skin
- * @typeded {string()} encodedString
- */
+Object.freeze(trssErrors);
 
 export class TrssError extends Error {
-  message = "TrssError";
+  name = "TrssError";
   /**@type {number} */
   code;
   /**
@@ -40,6 +98,22 @@ export class TrssError extends Error {
   constructor(errorCode) {
     super(trssErrors[errorCode]);
     this.code = errorCode;
+  }
+}
+
+export class SameSkinError extends Error {
+  name = "TrssError";
+  /**@type {{error_code: number, id: number}} */
+  obj;
+  /**
+   * @constructor
+   * @param {{error_code: number, id: number}} errorObject
+   */
+  constructor(errorObject) {
+    if (errorObject.error_code === 5 && "id" in errorObject) {
+      super(errorObject);
+    } else
+      throw new TypeError('Wrong object in the "SameSkinError" constructor');
   }
 }
 
@@ -57,8 +131,67 @@ function isJson(item) {
 }
 
 /**
+ * @param {string} skin
+ * @param {boolean} twoDimensional
+ * @returns {string[][]}
+ */
+export function decodeSkin(skin, twoDimensional = true) {
+  const bufdata = Buffer.from(skin.replace("trSkin1", "").trim(), "base64");
+  const binData = new Uint8Array(bufdata);
+  const compressed = inflateRawSync(binData);
+  const str = String.fromCharCode.apply(null, new Uint16Array(compressed));
+  const arr = str.split(";").map((e) => "#" + e + (e.length === 6 ? "FF" : ""));
+  arr.pop();
+  return twoDimensional ? chunk(arr, 20) : arr;
+}
+
+/**
+ * @param {any} skin
+ * @returns {boolean}
+ */
+export function isDecodedSkin(skin) {
+  return (
+    Array.isArray(skin) &&
+    skin.length === 18 &&
+    skin.every(
+      (e) =>
+        Array.isArray(e) &&
+        e.length === 20 &&
+        e.every(
+          (d) => typeof d == "string" && d.length < 10 && d.startsWith("#")
+        )
+    )
+  );
+}
+
+/**
+ * @param {string} skin
+ * @returns {string[][]}
+ */
+export function encodeSkin(skin) {
+  skin.push("");
+  return (
+    "trSkin1" +
+    deflateRawSync(
+      skin.flat().join(";").replace(/#/g, "").toUpperCase()
+    ).toString("base64")
+  );
+}
+
+/**
+ * @param {any} skin
+ * @returns {boolean}
+ */
+export function isEncodedSkin(skin) {
+  return isDecodedSkin(decodeSkin(skin));
+}
+
+/**
+ * @async
+ * @param {string} link
  * @param {string} action
  * @param {Object} params
+ * @returns {Promise<string>}
  */
 async function makeRequest(link, action, params) {
   const data = new URLSearchParams();
@@ -71,69 +204,440 @@ async function makeRequest(link, action, params) {
     },
     body: data,
   }).catch((e) => console.error(e));
-  return response.text();
+  return await response.text();
 }
 
-/**
- * @param {any} skin
- * @returns {boolean}
- */
-export function isSkin(skin) {
-  return (
-    Array.isArray(skin) &&
-    skin.length === 18 &&
-    skin.every(
-      (e) =>
-        Array.isArray(e) &&
-        e.length === 20 &&
-        e.every((d) => typeof d == "string" && d.length < 9)
-    )
-  );
-}
+// USERS.PHP
 
 /**
+ * @async
  * @param {string} login
  * @param {string} password
- * @returns {token}
+ * @returns {Promise<string>}
  */
 export async function login(login, password) {
-  if (typeof login != "string" && typeof password != "string") {
-    throw new Error('"login" and "password" is not string');
-  }
   if (typeof login != "string") {
-    throw new Error('"login" is not string');
+    throw new TypeError('"login" is not string');
   }
   if (typeof password != "string") {
-    throw new Error('"password" is not string');
+    throw new TypeError('"password" is not string');
   }
-  const res = await makeRequest(links.usersLink, "login", {
+  const res = await makeRequest(links.usersLink, actions.login, {
     login,
     password,
   });
   if (isJson(res) && "error_code" in JSON.parse(res)) {
-    throw new TrssError(res.error_code);
+    throw new TrssError(JSON.parse(res).error_code);
   }
   return res;
 }
 
-/*
+/**
+ * @async
  * @param {number} id
- * @rerurns {{id: number, login: string, skin: encodedSkin, primary_color: string, secondary_color: string}}
-*/
+ * @returns {Promise<UserInfo>}
+ */
 export async function getUserById(id) {
   id = Math.floor(id);
   if (isNaN(id)) {
-    throw new Error('"id" is not a number');
+    throw new TypeError('"id" is not a number');
   }
-  const req = await makeRequest(links.usersLink, actions.getUserById, {
-    id,
-  });
-  const res = JSON.parse(req);
+  const res = JSON.parse(
+    await makeRequest(links.usersLink, actions.getUserById, {
+      id,
+    })
+  );
   if ("error_code" in res) {
     throw new TrssError(res.error_code);
   }
   return res;
 }
 
-//TODO: write a function
-export async function getUserByToken(token) {}
+/**
+ * @async
+ * @param {string} token
+ * @returns {Promise<UserInfoByToken>}
+ */
+export async function getUserByToken(token) {
+  if (typeof token != "string") {
+    throw new TypeError('"token" is not a string');
+  }
+  const res = JSON.parse(
+    await makeRequest(links.usersLink, actions.getUserByToken, { token })
+  );
+  if ("error_code" in res) {
+    throw new TrssError(res.error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {string} token
+ * @param {string} newSkinEncoded
+ * @param {string} primaryColor
+ * @param {string} secondaryColor
+ * @returns {Promise<ChangedSkin>}
+ */
+export async function changeUserSkin(
+  token,
+  newSkinEncoded,
+  primaryColor,
+  secondaryColor
+) {
+  if (typeof token != "string") {
+    throw new TypeError('"token" is not a string');
+  }
+  if (!isEncodedSkin(newSkinEncoded)) {
+    throw new TypeError('"newSkinEncoded" is not a string');
+  }
+  primaryColor = chroma(primaryColor).hex().toUpperCase();
+  secondaryColor = chroma(secondaryColor).hex().toUpperCase();
+  const res = JSON.parse(
+    await makeRequest(links.usersLink, actions.changeUserSkin, {
+      token,
+      new_skin: newSkinEncoded,
+      primary_color: primaryColor,
+      secondary_color: secondaryColor,
+    })
+  );
+  if ("error_code" in res) {
+    throw new TrssError(res.error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {string} token
+ * @param {number} skinId
+ * @returns {Promise<number>} 1 if like is added else 0
+ */
+export async function toggleLike(token, skinId) {
+  if (typeof token != "string") {
+    throw new TypeError('"token" is not a string');
+  }
+  skinId = Math.floor(skinId);
+  if (isNaN(skinId)) {
+    throw new TypeError('"skinId" is not a number');
+  }
+  const res = await makeRequest(links.usersLink, actions.toggleLike, {
+    token,
+    skin_id: skinId,
+  });
+  if (isJson(res) && "error_code" in JSON.parse(res)) {
+    throw new TrssError(JSON.parse(res).error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} token
+ * @param {number} skinId
+ * @returns {Promise<number>} 1 if user viewed the skin else 0
+ */
+export async function registerView(token, skinId) {
+  if (typeof token != "string") {
+    throw new TypeError('"token" is not a string');
+  }
+  skinId = Math.floor(skinId);
+  if (isNaN(skinId)) {
+    throw new TypeError('"skinId" is not a number');
+  }
+  const res = await makeRequest(links.usersLink, actions.registerView, {
+    token,
+    skin_id: skinId,
+  });
+  if (isJson(res) && "error_code" in JSON.parse(res)) {
+    throw new TrssError(JSON.parse(res).error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} userId
+ * @param {number} skinId
+ * @returns {Promise<number>} 1 if user has liked
+ */
+export async function getLike(userId, skinId) {
+  userId = Math.floor(userId);
+  if (isNaN(userId)) {
+    throw new TypeError('"userId" is not a number');
+  }
+  skinId = Math.floor(skinId);
+  const res = await makeRequest(links.usersLink, actions.getLike, {
+    user_id: userId,
+    skin_id: skinId,
+  });
+  if (isJson(res) && "error_code" in JSON.parse(res)) {
+    throw new TrssError(JSON.parse(res).error_code);
+  }
+  return res;
+}
+
+// SKINS.PHP
+
+/**
+ * @async
+ * @param {string} token
+ * @param {string} encodedSkin
+ * @param {string} skinName
+ * @param {string} primaryColor
+ * @param {string} secondaryColor
+ * @returns {Promise<number>} 1 if the skin is written to the database else error
+ */
+export async function uploadSkin(
+  token,
+  encodedSkin,
+  skinName,
+  primaryColor,
+  secondaryColor
+) {
+  if (typeof token != "string") {
+    throw new TypeError('"token" is not a string');
+  }
+  if (!isEncodedSkin(encodedSkin)) {
+    throw new TypeError('"encodedSkin" is not an encoded skin');
+  }
+  primaryColor = chroma(primaryColor).hex().toUpperCase();
+  secondaryColor = chroma(secondaryColor).hex().toUpperCase();
+  const res = await makeRequest(links.skinsLink, actions.uploadSkin, {
+    token,
+    skin: encodedSkin,
+    skin_name: String(skinName),
+    primary_color: primaryColor,
+    secondary_color: secondaryColor,
+  });
+  if (isJson(res) && "id" in JSON.parse(res)) {
+    throw new SameSkinError(JSON.parse(res));
+  }
+  if (isJson(res) && "error_code" in JSON.parse(res)) {
+    throw new TrssError(JSON.parse(res).error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {string} token
+ * @param {number} skinId
+ * @returns {Promise<number>} 1 if the skin is removed from the database
+ */
+export async function removeSkin(token, skinId) {
+  if (typeof token != "string") {
+    throw new TypeError('"token" is not a string');
+  }
+  skinId = Math.floor(skinId);
+  if (isNaN(skinId)) {
+    throw new TypeError('"skinId" is not a number');
+  }
+  const res = await makeRequest(links.skinsLink, actions.removeSkin, {
+    token,
+    skin_id: skinId,
+  });
+  if (isJson(res) && "error_code" in JSON.parse(res)) {
+    throw new TrssError(JSON.parse(res).error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} skinId
+ * @returns {Promise<SkinInfo>} skin information
+ */
+export async function getSkinById(skinId) {
+  skinId = Math.floor(skinId);
+  if (isNaN(skinId)) {
+    throw new TypeError('"skinId" is not a number');
+  }
+  const res = JSON.parse(
+    await makeRequest(links.skinsLink, actions.getSkinById, {
+      skin_id: skinId,
+    })
+  );
+  if ("error_code" in res) {
+    throw new TrssError(res.error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} fromId
+ * @param {number} amount
+ * @returns {Promise<SkinInfo[]>}
+ */
+export async function getSkinsById(fromId, amount) {
+  fromId = Math.floor(fromId);
+  if (isNaN(fromId)) {
+    throw new TypeError('"fromId" is not a number');
+  }
+  amount = Math.floor(amount);
+  if (isNaN(amount)) {
+    throw new TypeError('"amount" is not a number');
+  }
+  const res = JSON.parse(
+    await makeRequest(links.skinsLink, actions.getSkinsById, {
+      from: fromId,
+      amount,
+    })
+  );
+  if (!Array.isArray(res) && "error_code" in res) {
+    throw new TrssError(res.error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} fromId
+ * @param {number} amount
+ * @returns {Promise<SkinInfo[]>}
+ */
+export async function getRecentSkins(fromId, amount) {
+  fromId = Math.floor(fromId);
+  if (isNaN(fromId)) {
+    throw new TypeError('"fromId" is not a number');
+  }
+  amount = Math.floor(amount);
+  if (isNaN(amount)) {
+    throw new TypeError('"amount" is not a number');
+  }
+  const res = JSON.parse(
+    await makeRequest(links.skinsLink, actions.getRecentSkins, {
+      from: fromId,
+      amount,
+    })
+  );
+  if (!Array.isArray(res) && "error_code" in res) {
+    throw new TrssError(res.error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} authorId
+ * @param {number} fromId
+ * @param {number} amount
+ * @returns {Promise<SkinInfo[]>}
+ */
+export async function getSkinsByAuthorId(authorId, fromId, amount) {
+  authorId = Math.floor(authorId);
+  if (isNaN(authorId)) {
+    throw new TypeError('"authorId" is not a number');
+  }
+  fromId = Math.floor(fromId);
+  if (isNaN(fromId)) {
+    throw new TypeError('"fromId" is not a number');
+  }
+  amount = Math.floor(amount);
+  if (isNaN(amount)) {
+    throw new TypeError('"amount" is not a number');
+  }
+  const res = JSON.parse(
+    await makeRequest(links.skinsLink, actions.getSkinsByAuthorId, {
+      author_id: authorId,
+      from: fromId,
+      amount,
+    })
+  );
+  if (!Array.isArray(res) && "error_code" in res) {
+    throw new TrssError(res.error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} fromId
+ * @param {number} amount
+ * @returns {Promise<SkinInfo[]>}
+ */
+export async function getSkinsByLikes(fromId, amount) {
+  fromId = Math.floor(fromId);
+  if (isNaN(fromId)) {
+    throw new TypeError('"fromId" is not a number');
+  }
+  amount = Math.floor(amount);
+  if (isNaN(amount)) {
+    throw new TypeError('"amount" is not a number');
+  }
+  const res = JSON.parse(
+    await makeRequest(links.skinsLink, actions.getSkinsByLikes, {
+      from: fromId,
+      amount,
+    })
+  );
+  if (!Array.isArray(res) && "error_code" in res) {
+    throw new TrssError(res.error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} fromId
+ * @param {number} amount
+ * @returns {Promise<SkinInfo[]>}
+ */
+export async function getSkinsByViews(fromId, amount) {
+  fromId = Math.floor(fromId);
+  if (isNaN(fromId)) {
+    throw new TypeError('"fromId" is not a number');
+  }
+  amount = Math.floor(amount);
+  if (isNaN(amount)) {
+    throw new TypeError('"amount" is not a number');
+  }
+  const res = JSON.parse(
+    await makeRequest(links.skinsLink, actions.getSkinsByViews, {
+      from: fromId,
+      amount,
+    })
+  );
+  if (!Array.isArray(res) && "error_code" in res) {
+    throw new TrssError(res.error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} skinId
+ * @returns {Promise<number>}
+ */
+export async function getLikes(skinId) {
+  skinId = Math.floor(skinId);
+  if (isNaN(skinId)) {
+    throw new TypeError('"skinId" is not a number');
+  }
+  const res = await makeRequest(links.skinsLink, actions.getLikes);
+  if (isJson(res) && "error_code" in JSON.parse(res)) {
+    throw new TrssError(JSON.parse(res).error_code);
+  }
+  return res;
+}
+
+/**
+ * @async
+ * @param {number} skinId
+ * @returns {Promise<number>}
+ */
+export async function getViews(skinId) {
+  skinId = Math.floor(skinId);
+  if (isNaN(skinId)) {
+    throw new TypeError('"skinId" is not a number');
+  }
+  const res = await makeRequest(links.skinsLink, actions.getViews);
+  if (isJson(res) && "error_code" in JSON.parse(res)) {
+    throw new TrssError(JSON.parse(res).error_code);
+  }
+  return res;
+}
+
+export const danger = { makeRequest };
+Object.freeze(danger);
